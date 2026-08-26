2bc4689fbb6a24f0346d521ee67f6594d74cc4de
vanisle35mm <blanchard_andre@live.ca>
2026-08-26 17:25:15 -0400

    Delete API/health.js

--- API/health.js
+++
@@ -1,7 +0,0 @@
-module.exports = async function handler(req, res) {
-  return res.status(200).json({
-    ok: true,
-    apiKeyConfigured: Boolean(process.env.OPENAI_API_KEY),
-    runtime: process.version,
-  });
-};
